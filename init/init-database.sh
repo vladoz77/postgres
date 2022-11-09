4ff65b38f5194a534e27ef3f1830fe39ca45e23c
Your Name <you@example.com>
2022-11-09 04:50:08 +0000

    edit init script

--- init/init-database.sh
+++ init/init-database.sh
@@ -2,9 +2,9 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER docker;
-    CREATE DATABASE my_project_development;
-    GRANT ALL PRIVILEGES ON DATABASE my_project_development TO docker;
-    CREATE DATABASE my_project_test;
-    GRANT ALL PRIVILEGES ON DATABASE my_project_test TO docker;
+    CREATE USER vlad;
+    CREATE DATABASE my_test;
+    GRANT ALL PRIVILEGES ON DATABASE my_test TO vlad;
+    
 EOSQL
+
